156923880477954c14caaa15c6130d3e0423986a
Tsutsumi,Yosuke | Oscar | TRVDD <yosuke.tsutsumi@rakuten.com>
2018-12-02 13:20:48 +0900

    update eslint rule

--- layouts/page.js
+++ layouts/page.js
@@ -1,7 +1,7 @@
-import React from 'react'
-import Header from '../components/header'
-import Footer from '../components/footer'
-import Sidebar from '../components/sidebar'
+import React from 'react';
+import Header from '../components/header';
+import Footer from '../components/footer';
+import Sidebar from '../components/sidebar';
 import styles from './page.scss';
 
 export default class AppPage extends React.Component {
@@ -15,29 +15,25 @@ export default class AppPage extends React.Component {
   onToggleSidebar = () => {
     this.setState({
       openSidebar: !this.state.openSidebar
-    })
+    });
     console.log('clicked');
-  }
+  };
 
   render() {
     return (
       <div>
         <Header headTitle={this.props.headTitle} onClickMenu={this.onToggleSidebar} />
-          <div className={styles.mainWrapper}>
-            <div className="ui container py-4">
-              { this.props.children }
-            </div>
-          </div>
+        <div className={styles.mainWrapper}>
+          <div className="ui container py-4">{this.props.children}</div>
+        </div>
         <Footer />
-        {
-          this.state.openSidebar && (
-            <div>
-              <div className={styles.sidebarDim} onClick={this.onToggleSidebar} />
-              <Sidebar />
-            </div>
-          )
-        }
+        {this.state.openSidebar && (
+          <div>
+            <div className={styles.sidebarDim} onClick={this.onToggleSidebar} />
+            <Sidebar />
+          </div>
+        )}
       </div>
-    )
+    );
   }
 }
