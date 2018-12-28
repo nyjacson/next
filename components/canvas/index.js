1c200984e01c6d77f1c1fdb3d0fdac6e947c5e69
Tsutsumi,Yosuke | Oscar | TRVDD <yosuke.tsutsumi@rakuten.com>
2018-12-28 12:37:46 +0900

    update drawLine

--- components/canvas/index.js
+++ components/canvas/index.js
@@ -17,14 +17,17 @@ export default class Canvas extends React.Component<Props, State> {
     this.canvasWrapperRef = React.createRef();
     this.state = {
       canvasWidth: 0,
-      canvasHeight: 0
+      canvasHeight: 0,
+      startX: 0,
+      startY: 0,
+      key: 0,
+      isDrawing: false
     };
   }
 
   componentDidMount() {
     this.onResize();
     window.addEventListener('resize', this.onResize);
-    this.canvasWrapperRef.current.addEventListener('click', console.log('test'));
     // const bgImg = new Image();
     // bgImg.src = '/static/img/top.jpg';
     // bgImg.width = this.state.canvasWidth;
@@ -56,25 +59,90 @@ export default class Canvas extends React.Component<Props, State> {
         canvasHeight: clientHeight
       },
       () => {
-        this.draw();
+        this.initializeCanvas();
       }
     );
   };
 
-  draw = () => {
+  initializeCanvas = () => {
     const ctx = this.getContext();
-    ctx.fillStyle = 'rgba(255, 255, 255, 0.8)';
+    ctx.globalCompositeOperation = 'source-over';
+    ctx.fillStyle = 'rgba(255, 255, 255, 0.85)';
     ctx.fillRect(0, 0, this.state.canvasWidth, this.state.canvasHeight);
+  };
+
+  mouseMove = e => {
+    this.setState({
+      isDrawing: true
+    });
+    if (this.state.isDrawing) {
+      this.drawLine(e);
+    }
+  };
+
+  mouseLeave = () => {
+    this.setState({
+      isDrawing: false,
+      startX: 0,
+      startY: 0
+    });
+  };
+
+  mouseEnter = e => {
+    const rect = e.target.getBoundingClientRect();
+    this.setState({
+      isDrawing: true,
+      startX: e.clientX - rect.left,
+      startY: e.clientY - rect.top
+    });
+  };
+
+  mouseDown = () => {
+    this.setState(
+      {
+        key: Math.random()
+      },
+      () => {
+        this.initializeCanvas();
+      }
+    );
+  };
+
+  drawLine = e => {
+    const rect = e.target.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+    const ctx = this.getContext();
+    ctx.globalCompositeOperation = 'destination-out';
+    ctx.lineWidth = 10;
+    ctx.lineCap = 'round';
+    ctx.beginPath();
+    ctx.moveTo(this.state.startX, this.state.startY);
+    ctx.lineTo(x, y);
+    ctx.stroke();
+    this.setState({
+      startX: x,
+      startY: y
+    });
+  };
+
+  loop = () => {
+    requestAnimationFrame(this.loop);
+    this.drawLine();
   };
 
   render() {
     return (
       <div className={styles.topbgImg} ref={this.canvasWrapperRef}>
         <canvas
+          key={this.state.key}
           ref={this.canvasRef}
           width={this.state.canvasWidth || 0}
           height={this.state.canvasHeight || 0}
-          onMouseEnter={() => console.log('down')}
+          onMouseEnter={e => this.mouseEnter(e)}
+          onMouseLeave={e => this.mouseLeave(e)}
+          onMouseMove={e => this.mouseMove(e)}
+          onMouseDown={() => this.mouseDown()}
         />
       </div>
     );
